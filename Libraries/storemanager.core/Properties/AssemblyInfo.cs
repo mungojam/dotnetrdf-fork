9615ae2df1a71bbf75f841da88412665c8b922f6
Mark Adamson <mark@adamsonfamily.co.uk>
2018-08-23 22:00:17 +0200

    convert using 75dd0ddc3e61613629ac8464bbdd1c6eec83716b of proj converter

--- Libraries/storemanager.core/Properties/AssemblyInfo.cs
+++ Libraries/storemanager.core/Properties/AssemblyInfo.cs
@@ -27,18 +27,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("StoreManager Core")]
-[assembly: AssemblyDescription("Core infrastructure for the Store Manager tool")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Visual Design Studios")]
-[assembly: AssemblyProduct("StoreManager.core")]
-[assembly: AssemblyCopyright("Copyright © dotNetRDF Project 2009-2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -46,19 +34,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f8efe8f7-d28d-48e4-8241-526eaf9f4974")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 
 
